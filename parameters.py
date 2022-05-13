00c6d0216ce9471a30f1ed29d036cf76674a52c5
nmarounina <nmarounina@gmail.com>
2022-05-13 21:33:48 +0200

    Putting functions and constants in place

---
+++ parameters.py
@@ -0,0 +1,13 @@
+
+# CRITICAL POINT
+Tc = 647.096 #K, temperature
+pc = 22.064e6 #Pa, pressure
+rhoc = 322./18.015268e-3 #mol.m-3
+
+# TRIPLE POINT
+Tt = 273.16 #K, temperature
+pt = 611.655 #Pa, pressure
+
+# GENERAL PARAMETERS OF WATER
+Rig = 8.3143714 # ideal gas constant
+M_h2o = 18.015268e-3 #kg.mol-1, molar mass
