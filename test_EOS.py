8e3eed682aa87bc824ef5dc38651d71768936c4e
nmarounina <nmarounina@gmail.com>
2022-06-01 09:46:37 +0200

    Display of additional parameters

--- test_EOS.py
+++ test_EOS.py
@@ -3,4 +3,7 @@ import IAPWS95
 
 
 point=IAPWS95.DataPoint( 700, 22.5e6 )
-print("Density =" , point.rho*M_h2o, "kg.m-3" )
+print( " %s %f %s" %("Density =",  point.rho*M_h2o, "kg.m-3" ))
+print(" %s %f %s" %("Entropy =" , point.s, "J.mol-1.K-1"))
+print(" %s %f %s" %("Heat capacity =" , point.Cp, "J.mol-1.K-1"))
+print(" %s %f %s" %("Internal enerjy =" , point.u, "J.mol-1"))
